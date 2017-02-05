5099d721bdf023c022d5982228c51f0d4bf4140d
KazumiHarada <kazumi.h.apple48@gmail.com>
2017-02-05 22:39:14 +0900

    te

--- app/src/main/java/com/android/kharada/designsample/activity/BottomSheetActivity.java
+++ app/src/main/java/com/android/kharada/designsample/activity/BottomSheetActivity.java
@@ -1,19 +1,7 @@
 package com.android.kharada.designsample.activity;
 
-import android.animation.Animator;
-import android.os.Build;
-import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewAnimationUtils;
-import android.view.ViewTreeObserver;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.android.kharada.designsample.R;
 
@@ -22,15 +10,12 @@ public class BottomSheetActivity extends ListItemActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
     }
 
     @Override
     protected int getLayoutResId() {
         return R.layout.activity_bottom_sheet;
     }
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
