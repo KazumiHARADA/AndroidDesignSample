9caa4075d3280ff16116c556bcab8d15f63c5684
KazumiHarada <kazumi.h.apple48@gmail.com>
2017-02-05 14:41:39 +0900

    te

--- app/src/main/java/com/android/kharada/designsample/activity/BottomSheetActivity.java
+++ app/src/main/java/com/android/kharada/designsample/activity/BottomSheetActivity.java
@@ -1,67 +1,45 @@
 package com.android.kharada.designsample.activity;
 
+import android.animation.Animator;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.Button;
+import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.android.kharada.designsample.R;
-import com.github.ksoichiro.android.observablescrollview.ObservableScrollView;
-import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
-import com.github.ksoichiro.android.observablescrollview.ScrollState;
-import com.github.ksoichiro.android.observablescrollview.ScrollUtils;
 
-public class BottomSheetActivity extends AppCompatActivity implements ObservableScrollViewCallbacks {
+public class BottomSheetActivity extends AppCompatActivity {
 
     ImageView mHeaderImage;
-    TextView mHeaderText;
+    CollapsingToolbarLayout mHeader;
     TextView mDescriptionText;
-    LinearLayout mParallaxArea;
-    Button mExecButton;
-
     Toolbar mToolbar;
-
-    ObservableScrollView mScrollView;
-
-    private int mParallaxHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_sheet);
 
-        mHeaderImage = (ImageView) findViewById(R.id.image_header);
-        mHeaderText = (TextView) findViewById(R.id.text_header);
-        mDescriptionText = (TextView) findViewById(R.id.text_description);
-        mExecButton = (Button) findViewById(R.id.btn_exec);
-        mParallaxArea = (LinearLayout)findViewById(R.id.parallax_area);
-
+        mHeaderImage = (ImageView) findViewById(R.id.backdrop);
+        mHeader = (CollapsingToolbarLayout) findViewById(R.id.header);
+        mDescriptionText = (TextView) findViewById(R.id.overview_description);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        mScrollView = (ObservableScrollView) findViewById(R.id.scrollView);
 
         Bundle bundle = getIntent().getExtras();
 
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
-        getSupportActionBar().setTitle(bundle.getString("title","title"));
+        getSupportActionBar().setTitle(bundle.getString("title", "title"));
 
-        mHeaderText.setText(bundle.getString("title",""));
-        mDescriptionText.setText(bundle.getString("description",""));
-
-        mScrollView.setScrollViewCallbacks(this);
-
-        // Toolbarの背景色をalpha=0でセットする
-        mToolbar.setBackgroundColor(ScrollUtils.getColorWithAlpha(0, getResources().getColor(R.color.colorPrimary)));
-        mToolbar.setTitleTextColor(ScrollUtils.getColorWithAlpha(0, getResources().getColor(R.color.colorPrimary)));
-
-        // 画像領域の高さを取得
-        mParallaxHeight = getResources().getDimensionPixelSize(R.dimen.parallax_image_height);
-
+        mHeader.setTitle(bundle.getString("title", ""));
+        mDescriptionText.setText(bundle.getString("description", ""));
     }
 
     @Override
@@ -84,30 +62,21 @@ public class BottomSheetActivity extends AppCompatActivity implements Observable
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        onScrollChanged(mScrollView.getCurrentScrollY(), false, false);
     }
 
-    @Override
-    public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
-        int baseColor = getResources().getColor(R.color.colorPrimary);
-        int baseTextColor = getResources().getColor(android.R.color.white);
+    private void circularRevealActivity(View rootLayout) {
 
-        // 現在のスクロール量からalpha値を計算する(0~1.0)
-        float alpha = Math.min(1, (float) scrollY / mParallaxHeight);
-        mToolbar.setBackgroundColor(ScrollUtils.getColorWithAlpha(alpha, baseColor));
-        mToolbar.setTitleTextColor(ScrollUtils.getColorWithAlpha(alpha, baseTextColor));
+        int cx = rootLayout.getWidth() / 2;
+        int cy = rootLayout.getHeight() / 2;
 
-        mHeaderImage.setTranslationY(scrollY / 2);
-        mParallaxArea.setTranslationY(scrollY / 2);
-    }
+        float finalRadius = Math.max(rootLayout.getWidth(), rootLayout.getHeight());
 
-    @Override
-    public void onDownMotionEvent() {
+        // create the animator for this view (the start radius is zero)
+        Animator circularReveal = ViewAnimationUtils.createCircularReveal(rootLayout, cx, cy, 0, finalRadius);
+        circularReveal.setDuration(1000);
 
-    }
-
-    @Override
-    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
-
+        // make the view visible and start the animation
+        rootLayout.setVisibility(View.VISIBLE);
+        circularReveal.start();
     }
 }
--- app/src/main/java/com/android/kharada/designsample/activity/MainActivity.java
+++ app/src/main/java/com/android/kharada/designsample/activity/MainActivity.java
@@ -1,8 +1,7 @@
 package com.android.kharada.designsample.activity;
 
 import android.content.Intent;
-import android.content.res.AssetManager;
-import android.support.design.widget.TabLayout;
+
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,27 +10,14 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-import com.android.kharada.designsample.DesignSampleApplication;
 import com.android.kharada.designsample.R;
 import com.android.kharada.designsample.adapter.MainAdapter;
 import com.android.kharada.designsample.gson.MainListModel;
 import com.android.kharada.designsample.util.AssetUtil;
 import com.google.gson.Gson;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -59,8 +45,8 @@ public class MainActivity extends AppCompatActivity {
             try {
                 Class activityClass = Class.forName(item.getActivityName());
                 Intent intent = new Intent(this, activityClass);
-                intent.putExtra("title",item.getTitle());
-                intent.putExtra("description",item.getDescription());
+                intent.putExtra("title", item.getTitle());
+                intent.putExtra("description", item.getDescription());
                 startActivity(intent);
             } catch (ClassNotFoundException e) {
                 return;
@@ -77,14 +63,5 @@ public class MainActivity extends AppCompatActivity {
         mSearchView = (SearchView) MenuItemCompat.getActionView(searchItem);
 
         return true;
-    }
-
-
-    private List<String> createData() {
-        List<String> data = new ArrayList<>();
-        for (int i = 0; i < 20; i++) {
-            data.add(String.valueOf(i));
-        }
-        return data;
     }
 }
